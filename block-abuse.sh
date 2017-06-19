e63bddc137b6d5c6cd374ac45e02e7fd3a275f4e
Tony Esterdark <messi.klate@gmail.com>
2017-06-19 21:40:27 +0800

    Delete block-abuse.sh

--- block-abuse.sh
+++
@@ -1,5 +0,0 @@
-iptables -A OUTPUT -d vps-termurah.net -j DROP
-iptables -A OUTPUT -d account.sonyentertainmentnetwork.com -j DROP
-iptables -A OUTPUT -d auth.np.ac.playstation.net -j DROP
-iptables -A OUTPUT -d auth.api.sonyentertainmentnetwork.com -j DROP
-iptables -A OUTPUT -d auth.api.np.ac.playstation.net -j DROP
