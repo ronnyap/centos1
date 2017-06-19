914267db0f7ce0c404e909a7780ba1d858d06c30
Tony Esterdark <messi.klate@gmail.com>
2017-06-19 21:33:18 +0800

    Add files via upload

---
+++ block-abuse.sh
@@ -0,0 +1,5 @@
+iptables -A OUTPUT -d vps-termurah.net -j DROP
+iptables -A OUTPUT -d account.sonyentertainmentnetwork.com -j DROP
+iptables -A OUTPUT -d auth.np.ac.playstation.net -j DROP
+iptables -A OUTPUT -d auth.api.sonyentertainmentnetwork.com -j DROP
+iptables -A OUTPUT -d auth.api.np.ac.playstation.net -j DROP
